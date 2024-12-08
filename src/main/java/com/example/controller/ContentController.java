2084c7544ba2be93a29b1173fe08f9541d9f8845
nenowo <38612806+nenowoDev@users.noreply.github.com>
2024-12-08 14:16:07 +0800

    added content

---
+++ src/main/java/com/example/controller/ContentController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/content")
+public class ContentController {
+
+	@RequestMapping("/view")
+	public ModelAndView viewContent(){
+		ModelAndView mav=new ModelAndView("content/view");
+		
+		return mav;
+	}
+	
+	
+	
+}
+
---
+++ src/main/java/com/example/model/Content.java
@@ -0,0 +1,7 @@
+package com.example.model;
+
+public class Content {
+	private String contentTitle;
+	private String link;
+	
+}
