5f2ff5c0491e5011fdb417750d8c8cfd89ffc6c2
nenowo <38612806+nenowoDev@users.noreply.github.com>
2024-12-08 19:53:13 +0800

    a

--- src/main/java/com/example/model/Content.java
+++ src/main/java/com/example/model/Content.java
@@ -1,7 +1,21 @@
 package com.example.model;
 
 public class Content {
+	private String contentID;
 	private String contentTitle;
 	private String link;
+	
+	public void setContentTitle(String title) {
+		this.contentTitle=title;
+	}
+	
+	public void setLink(String link) {
+		this.link=link;
+	}
+	
+	public getLink() {
+		
+	}
+	
 	
 }
